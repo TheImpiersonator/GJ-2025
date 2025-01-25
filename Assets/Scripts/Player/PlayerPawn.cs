4ce26dfa8432e03f238ca7051dc14ade11428330
Pierson <CleverCreator458@gmail.com>
2025-01-25 15:41:16 -0700

    Updated movement

--- Assets/Scripts/Player/PlayerPawn.cs
+++ Assets/Scripts/Player/PlayerPawn.cs
@@ -8,6 +8,8 @@ using UnityEngine;
     (1/25/2025)
     - Wanting to test out how it feels to restrain the vertical movement during strafes to better define the horizontal movement
     |__ Edits made in Update() | Created strafeMove() method | Made new float for restraining speed when strafing]
+    - Changed back to old method, implemented simpler math for strafe addition.
+        still had to use an if-else because unity is dumb and apparently hates simple vector calculations.
  */
 
 [RequireComponent(typeof(CharacterController))]
@@ -17,59 +19,59 @@ public class PlayerPawn : Pawn {
     private Vector3 vertVector;
     private Vector3 horizVector;
     private Vector3 turnVector;
+    private float lerpTime;
 
-    [Tooltip ("Restrains vertical movement by percentage during strafes")]
+    [Tooltip("Restrains vertical movement by percentage during strafes")]
     [SerializeField] float strafeRestraint; //Added for hidden strafing speed mechaninc
+    [SerializeField] float smoothness;
 
     void Start() {
         unityController = GetComponent<CharacterController>();
     }
 
     void Update() {
-        //normalized total movment direction
-        Vector3 moveVector = Vector3.Normalize(vertVector + horizVector);
+        //vector for pawn movement
+        Vector3 moveVector;
 
-        //__TWO MOVEMENT SITUATIONS {Non-Strafe & Strafe}__
-        //|__CHECK: There is Horizontal Input
+        //reduce vertical move vector when strafing
         if (horizVector.magnitude > 0) {
-            strafeMove();
+            //scaling vector by restraint multiplier
+            Vector3 newVec = vertVector * strafeRestraint;
+            //normalized total movment direction (scaled movement)
+            moveVector = Vector3.Normalize(newVec + horizVector);
         }
-        //|__ELSE: Move Normally
-        else{
-            //move player
-            unityController.Move(moveVector * moveSpeed * Time.deltaTime);
+        else {
+            //normalized total movment direction (raw movment)
+            moveVector = Vector3.Normalize(vertVector + horizVector);
         }
 
-        
+
+
+        //move player
+        unityController.Move(moveVector * moveSpeed * Time.deltaTime);
+
         //rotate player
         transform.Rotate(turnVector * turnSpeed * Time.deltaTime);
-    }
-
-    void strafeMove() {
-        //Get the adjusted speed for vertical movement
-        float adjSpeed = moveSpeed * strafeRestraint;
-        // Normalize horizontal movement to maintain direction
-        Vector3 normalizedHorizontal = horizVector.normalized;
-        // Move player using the horizontal input at full speed
-        unityController.Move(normalizedHorizontal * moveSpeed * Time.deltaTime);
-        // Apply vertical movement with restraint
-        unityController.Move(vertVector * adjSpeed * Time.deltaTime);
     }
 
     public override void MoveVertical(float moveDir) {
         //using forward direciton * decided movement float
         vertVector = moveDir * transform.forward;
-
     }
 
     public override void MoveHorizontal(float moveDir) {
-        
         //using right direciton * provided input float
         horizVector = moveDir * transform.right;
     }
 
     public override void RotatePawn(float rotDir) {
         turnVector = new Vector3(0, rotDir, 0);
+    }
+
+    private Vector3 SmoothMovement(Vector3 inVec) {
+        Vector3 workingVec = Vector3.zero;
+
+        return workingVec;
     }
 
 }
