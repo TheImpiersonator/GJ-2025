fb5df4ca3eb8f77fadb2a35e9ea5c391cb5f9f42
Kyler Burson <kyler.burson@gmail.com>
2025-01-26 12:34:03 -0700

    Menu Work

--- Assets/UI/MainMenu/Controls_Overlay.cs
+++ Assets/UI/MainMenu/Controls_Overlay.cs
@@ -4,10 +4,5 @@ using UnityEngine;
 
 public class Controls_Overlay : UI_Overlay
 {
-    [SerializeField] MainMenu_Overlay mainmenuUIPrefab;
-
-    public void pressed_Back()
-    {
-        GameManager.Instance.get_UIMaster().OpenOverlay<MainMenu_Overlay>(mainmenuUIPrefab.gameObject);
-    }
+    
 }
--- Assets/UI/MainMenu/Credits_Overlay.cs
+++ Assets/UI/MainMenu/Credits_Overlay.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class Credits_Overlay : UI_Overlay
 {
-    //copied from MainMenu_Overlay with terms changed from Credits to MainMenu
-    [SerializeField] MainMenu_Overlay mainmenuUIPrefab;
-
-    public void pressed_Back()
-    {
-        GameManager.Instance.get_UIMaster().OpenOverlay<MainMenu_Overlay>(mainmenuUIPrefab.gameObject);
-    }
+   
 }
