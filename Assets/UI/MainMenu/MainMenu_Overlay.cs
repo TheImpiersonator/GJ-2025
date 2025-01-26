fdce6b3f028008542c3df154d1628dfca135c7d4
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2025-01-26 09:40:36 -0700

    Made some fixes from merge

--- Assets/UI/MainMenu/MainMenu_Overlay.cs
+++ Assets/UI/MainMenu/MainMenu_Overlay.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class MainMenu_Script : UI_Overlay
 {
+    [SerializeField] Credits_Overlay creditsUIPrefab;
+    [SerializeField] Controls_Overlay controlsUIPrefab;
 
+    public void pressed_Start()
+    {
+        GameManager.Instance.StartGame();
+    }
+
+    public void pressed_Credits()
+    {
+        GameManager.Instance.get_UIMaster().OpenOverlay<Credits_Overlay>(creditsUIPrefab.gameObject);
+    }
+
+    public void pressed_Controls()
+    {
+        GameManager.Instance.get_UIMaster().OpenOverlay<Controls_Overlay>(controlsUIPrefab.gameObject);
+    }
+
+    public void pressed_Quit()
+    {
+        Application.Quit();
+    }
 }
