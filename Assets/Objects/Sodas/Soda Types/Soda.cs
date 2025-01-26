4a82773451585b565ea51239f4e681411fae9f21
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2025-01-25 22:19:23 -0700

    Soda Stats Logic but better?

--- Assets/Objects/Sodas/Soda Types/Soda.cs
+++ Assets/Objects/Sodas/Soda Types/Soda.cs
@@ -1,10 +1,10 @@
 using UnityEngine.Events;
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Soda : MonoBehaviour
 {
-    //====| EVENTS |====
+    //====| EVENTS |==== //SOME EVENTS MAY HAVE TO BE TURNED INTO DELEGATES IF NEEDED SUBSCRIPTIONS
     public UnityEvent OnShoot;
     public UnityEvent OnDrink;
     public delegate void DurationEndCall();
@@ -12,35 +12,92 @@ public abstract class Soda : MonoBehaviour
     public delegate void ThrowCall();
     public ThrowCall OnThrow;
     public UnityEvent OnExplode;
+    public delegate void LevelUpCall(int level);
+    public LevelUpCall OnLevelUp;
+    public delegate void RankUpCall(FIZZ_RANKS rank);
 
     //====| VARIABLES |====
-    //___Shoot
-    public float damage;        //Damage Output to enemy/objects per fire rate
-    public float shootRange;    //Range for the damage RayCast
-    public float spread;        //Spread Angle of the damage raycasting
-    public float fireRate;      //Rate at which damage is outputted
-    public float useDuration;      //For time it takes to fire or drink
+    //SODA-STATS Struct
+    [System.Serializable]
+    struct sodaStats {
+        //___Shoot Stats
+        public float damage;        //Damage Output to enemy/objects per fire rate
+        public float shootRange;    //Range for the damage RayCast
+        public float spread;        //Spread Angle of the damage raycasting
+        public float fireRate;      //Rate at which damage is outputted
+        public float useDuration;      //For time it takes to fire or drink
 
-    //___Throw
-    public float throwStrength;     //Strength of the throw force
-    public float throwAngle;        //Angle that the soda is tossed
-    public float explodeDistance;   //AOE explosion distance
+        //___Throw
+        public float throwStrength;     //Strength of the throw force
+        public float throwAngle;        //Angle that the soda is tossed
+        public float explodeDistance;   //AOE explosion distance
+    }
+
+    [Tooltip("Add a new set to include a new Level (stat set) to the Soda")]
+    [SerializeField] List<sodaStats> levelSets = new List<sodaStats>();
+
 
     //___Shake
-    //[Still wanting to talk about how the shaking works]
-    public float currShakePercent;  //
-    protected int MaxShakeLevel;    //Max amount of Levels ups the soda has
-    public int shakeLevel;          //The current
+    [SerializeField] float Max_ShakeAmount;
+    float curr_ShakeAmount;
+    public int shakeLevel;          //The current Level of the Soda
+    public enum FIZZ_RANKS {Stale, Carbonated, FIZZED}; //RANK OF THE SODA Based on Shame Amount [Should effect the Explosion stats]
+    public FIZZ_RANKS fizzRank = FIZZ_RANKS.Stale;      //Current Fizz Ranking
 
-    public void Shoot() { }
+    [System.Serializable]
+    struct RankMultipliers {
+        public float staleMod;
+        public float carbMod;
+        public float fizzedMod;
+    }
+
+
+    //====| UNITY SCHEDULE |=====
+
+    //====| METHODS |====
+    public void Shoot() {
+        ShootEffect();
+    }
     public void Throw() { }
-    public void Shake() { }
+    public void AddShake(float shakeAmount){
+        curr_ShakeAmount += shakeAmount;
+        UpdateRank();   //Attempt to Rank up
+        UpdateLevel();  //Attempt to Level up 
+    }
 
     protected virtual void ShootEffect() { 
-        
+        //Going just by base stats depending on the level
     }
 
     protected virtual void ExplodeEffect() { 
-        
+        //Stat = Level's value * the Rank Modifier :)
+    }
+    //Don't Mind this mess of a function :)
+    void UpdateRank()
+    {
+        float percent = get_ShakePercent();
+
+        //Stale Rank - Lower Shake percent
+        if (percent < (1 / 3) && fizzRank != FIZZ_RANKS.Stale)
+        { fizzRank = FIZZ_RANKS.Stale; }
+        //Carbonated Rank - Mid shake Percent
+        else if ((1 / 3) < percent && percent < (2 / 3) && fizzRank != FIZZ_RANKS.Carbonated)
+        { fizzRank = FIZZ_RANKS.Carbonated; }
+        //FIZZ RANK - High Shake Percent
+        else if (percent > (2 / 3) && fizzRank != FIZZ_RANKS.FIZZED)
+        { fizzRank = FIZZ_RANKS.FIZZED; }
+
+        else {
+            Debug.Log("====| COULDN'T DETECT FIZZ RANK |====");
+            fizzRank = FIZZ_RANKS.FIZZED;
+        }
+    }
+
+    void UpdateLevel() {
+        shakeLevel = (int)(levelSets.Count * get_ShakePercent());
+    }
+
+    public float get_ShakePercent() {
+        return Mathf.Clamp01(curr_ShakeAmount/Max_ShakeAmount);
     }
 }
